cef79cb343d03db47a3fe022283220501118ca6e
Clinton Ogada <clintonogada@gmail.com>
2023-02-24 10:35:21 +0300

    Update My-Birthday-Card2.kt
    Created a 'Column' to overlap Texts.
    Created a 'Box' to overlap text with Image

--- My-Birthday-Card2.kt
+++ My-Birthday-Card2.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
@@ -11,6 +12,7 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
@@ -26,24 +28,35 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    BirthdayGreetingWithText(message ="Happiest Birthday Dad", from ="From:Klint")
+                    BirthdayGreetingWithImage(message ="Happiest Birthday Vivian", from ="From:Klint")
                 }
             }
         }
     }
 }
 @Composable
-fun BirthdayGreetingWithImage(message:String, from: String){
+fun BirthdayGreetingWithImage(message:String, from: String, modifier:Modifier = Modifier){
 val image = painterResource(id = R.drawable.birthdayparty)
-    Image(painter = image, contentDescription = null )
+    //Create a box to overlap image and text
+    Box() {
+
+        Image(painter = image, contentDescription = null, contentScale= ContentScale.Crop )
+        BirthdayGreetingWithText(message ="Happiest Birthday Vivian", from ="From:Klint")
+    }
+
 
 }
 
 @Composable
 fun BirthdayGreetingWithText(message: String, from: String){
     Column {
-        Text(text = message, fontSize =25.sp)
-        Text(text = from, fontSize = 15.sp)
+        Text(
+            text = message, fontSize =25.sp
+        )
+        Text(
+            text = from, fontSize = 15.sp
+        )
+
     }
 
 }
@@ -52,6 +65,6 @@ fun BirthdayGreetingWithText(message: String, from: String){
 @Composable
 fun BirthdayGreetingWithImagePreview() {
     MyBirthdayCardTheme {
-        BirthdayGreetingWithImage(message ="Happiest Birthday Dad", from ="From: Klint")
+        BirthdayGreetingWithImage(message ="Happiest Birthday Vivian", from ="From: Klint")
     }
 }
